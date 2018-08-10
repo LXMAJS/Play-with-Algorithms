5bc2c966d852967affe7029d28483669ca3e6d8e
LXMAJS <lxmajs@163.com>
2018-08-10 15:53:56 +0800

    completed merge sort and its plus version

--- cpps/demo1/SortAlogrithms.h
+++ cpps/demo1/SortAlogrithms.h
@@ -27,17 +27,12 @@ namespace SortAlogrithms {
         }
     }
 
-
     // insertion sort
     template <typename T>
     void insertionSort(T arr[], int n){
         for (int i = 0; i < n; i++) {
-            for (int j = i; j > 0; j--) {
-                if (arr[i] < arr[j - 1])
-                    swap(arr[i], arr[j - 1]);
-                else
-                    break;
-            }
+            for( int j = i ; j > 0 && arr[j] < arr[j-1] ; j -- )
+                swap( arr[j] , arr[j-1] );
         }
     }
 
@@ -53,7 +48,24 @@ namespace SortAlogrithms {
             arr[j] = tempValue;
         }
     }
-    
+
+    // insertion sort, can sort range from left to right
+    template <typename T>
+    void insertionSortPlus(T arr[], int leftPos, int rightPos){
+
+        if(leftPos > rightPos)
+            return;
+
+        for (int i = leftPos; i < rightPos; ++i) {
+            T tempValue = arr[i];
+            int j;
+            for (j = i; j > leftPos && tempValue < arr[j - 1]; j --) {
+                arr[j] = arr[j - 1];
+            }
+            arr[j] = tempValue;
+        }
+    }
+    // bubble sort
     template <typename T>
     void bubbleSort(T arr[], int n){
 
@@ -63,6 +75,72 @@ namespace SortAlogrithms {
                     swap(arr[j], arr[j + 1]);
             }
         }
+    }
+
+
+
+
+
+    template <typename T>
+    void __mergeArray(T arr[], int leftPos, int middlePos, int rightPos) {
+        int n = rightPos - leftPos + 1;
+        int * newArray = new T[n];
+
+        int pos1 = leftPos;
+        int pos2 = middlePos + 1;
+        for (int i = 0; i < n; i++) {
+
+            if(pos1 > middlePos) {
+                newArray[i] = arr[pos2];
+                pos2 ++;
+            }
+            else if(pos2 > rightPos) {
+                newArray[i] = arr[pos1];
+                pos1 ++;
+            }
+            else if(arr[pos1] < arr[pos2]) {
+                newArray[i] = arr[pos1];
+                pos1++;
+            }
+            else {
+                newArray[i] = arr[pos2];
+                pos2++;
+            }
+        }
+
+        for (int j = 0; j < n; j ++) {
+            arr[leftPos + j] = newArray[j];
+        }
+    }
+
+    // merge sort private function
+    template <typename T>
+    void __mergeSort(T arr[], int leftPos, int rigthPos) {
+
+        //if(leftPos >= rigthPos)
+        //   return;
+
+        // use insertion sort when number in range less then 15, the "15" is any other number you like
+        if(rigthPos - leftPos <= 15){
+            insertionSortPlus(arr, leftPos, rigthPos);
+            return;
+        }
+
+        //warn!!! dangerous of memory overflow
+        int middlePos = (rigthPos + leftPos)/2;
+
+        __mergeSort(arr, leftPos, middlePos);
+        __mergeSort(arr, middlePos + 1, rigthPos);
+
+        // merge when left part of array has any items bigger then right part of array
+        if(arr[middlePos] > arr[middlePos + 1])
+            __mergeArray(arr, leftPos, middlePos, rigthPos);
+    }
+
+    // merge sort public function
+    template <typename T>
+    void mergeSort(T arr[], int n){
+        __mergeSort(arr, 0, n-1);
     }
 }
 
--- cpps/demo1/main.cpp
+++ cpps/demo1/main.cpp
@@ -6,19 +6,22 @@ using namespace std;
 
 int main() {
 
-    int n = 10000;
-    int* arr_1 = SortTestHelper::generateNearlyOrderArray(n, 100);
+    int n = 50000;
+    int* arr_1 = SortTestHelper::generateNearlyOrderArray(n, 0);
     int* arr_2 = SortTestHelper::copyIntArray(arr_1, n);
-    int* arr_3 = SortTestHelper::copyIntArray(arr_1, n);
+    //int* arr_3 = SortTestHelper::copyIntArray(arr_1, n);
 
-    SortTestHelper::testSort("Selection Sort", SortAlogrithms::selectionSort, arr_1, n);
+    //SortTestHelper::testSort("Selection Sort", SortAlogrithms::selectionSort, arr_1, n);
 
-    SortTestHelper::testSort("Insertion Sort Plus", SortAlogrithms::insertionSortPlus, arr_2, n);
+    SortTestHelper::testSort("Insertion Sort", SortAlogrithms::insertionSort, arr_2, n);
 
-    SortTestHelper::testSort("Bubble Sort", SortAlogrithms::bubbleSort, arr_3, n);
+    //SortTestHelper::testSort("Bubble Sort", SortAlogrithms::bubbleSort, arr_3, n);
+
+    SortTestHelper::testSort("Merge Sort", SortAlogrithms::mergeSort, arr_1, n);
+    //SortTestHelper::printArray(arr_1, n);
 
     delete[] arr_1;
     delete[] arr_2;
-    delete[] arr_3;
+    //delete[] arr_3;
     return 0;
 }
