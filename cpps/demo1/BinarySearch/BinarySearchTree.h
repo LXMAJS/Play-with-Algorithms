bc2fd750b8d11157e4e879b8a3f8cd99a7760ef8
LXMAJS <lxmajs@163.com>
2018-08-21 11:00:12 +0800

    [bst] completed bsts search and contain function

--- cpps/demo1/BinarySearch/BinarySearchTree.h
+++ cpps/demo1/BinarySearch/BinarySearchTree.h
@@ -30,25 +30,6 @@ namespace BinarySearchTree{
         Node * root; // root node
         int count; // tree node's count
 
-        /// insert a new item, private function
-        Node insert(Node node, Key k, Value val){
-
-            if(node == NULL){
-                count ++;
-                return new Node(k, val);
-            }
-
-            if(node.key == k){
-                node.value = val;
-            }
-            else if(node.key < k){
-                node.left = insert(node.left, k, val);
-            }
-            else {// node.key > k
-                node.right = insert(node.right, k, val);
-            }
-        }
-
     public:
 
         BST(){
@@ -70,41 +51,98 @@ namespace BinarySearchTree{
             return root == NULL;
         }
 
-//        /// insert a new item, use interation
-//        void insert(Key k, Value val){
-//            root = insert(root, k, val);
-//        }
+        /// insert a new item, use interation
+        void insert(Key k, Value val){
+            root = insert(root, k, val);
+        }
 
         /// insert a new item, without interation
-        void insert(Key k, Value val){
-            Node node = root;
-            
+//        void insert(Key k, Value val){
+//            Node node = root;
+//
+//            if(node == NULL){
+//                node = new Node(k, val);
+//                return;
+//            }
+//
+//            while(node != NULL){
+//                if(node.key == k){
+//                    node.value = val;
+//                }
+//                else if(node.key < k){
+//                    if(node.left == NULL){
+//                        node->left = new Node(k, val);
+//                        break;
+//                    }
+//                    node = node->left;
+//                }
+//                else{
+//                    // node.key > k
+//                    if(node.right == NULL){
+//                        node->right = new Node(k, val);
+//                        break;
+//                    }
+//                    node = node->right;
+//                }
+//            }
+//        }
+
+        /// if bst has a value'key is k
+        bool contain(Key k){
+            return contain(root, k);
+        }
+
+        /// find an value's address when it's key eqaul to k
+        Value* search(Key k){
+            return search(root, k);
+        }
+
+    private:
+        /// insert a new item, private function
+        Node insert(Node node, Key k, Value val){
+
             if(node == NULL){
-                node = new Node(k, val);
-                return;
+                count ++;
+                return new Node(k, val);
             }
 
-            while(node != NULL){
-                if(node.key == k){
-                    node.value = val;
-                }
-                else if(node.key < k){
-                    if(node.left == NULL){
-                        node.left = new Node(k, val);
-                        break;
-                    }
-                    node = node.left;
-                }
-                else{
-                    // node.key > k
-                    if(node.right == NULL){
-                        node.right = new Node(k, val);
-                        break;
-                    }
-                    node = node.right;
-                }
+            if(node.key == k){
+                node.value = val;
+            }
+            else if(node.key < k){
+                node.left = insert(node.left, k, val);
+            }
+            else {// node.key > k
+                node.right = insert(node.right, k, val);
             }
         }
+
+        ///
+        bool contain(Node* node, Key k){
+            if(node == NULL)
+                return false;
+
+            if(node->key == k)
+                return true;
+            else if(node->key < k)
+                return contain(node->left, k);
+            else
+                return contain(node->right, k);
+        }
+
+        ///
+        Value* search(Node* node, Key k){
+            if(node == NULL)
+                return NULL;
+
+            if(node->key == k)
+                return &(node->value);
+            else if(node->key < k)
+                return search(node->left, k);
+            else
+                return search(node->right, k);
+        }
+
     };
 }
 #endif //DEMO1_BINARYSEARCHTREE_H
