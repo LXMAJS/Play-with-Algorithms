544fc36b41b2bd0d16d9c4fbd9ac13a174c780a2
LXMAJS <lxmajs@163.com>
2018-09-13 15:25:44 +0800

    weight graph, compeleted class edge

---
+++ cpps/demo1/WeightGraph/DenseGraph.h
@@ -0,0 +1,128 @@
+//
+// Created by 黎进 on 2018/8/31.
+//
+
+#ifndef DEMO1_DENSEGRAPH_H
+#define DEMO1_DENSEGRAPH_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+namespace WeightGraph {
+
+    /*
+     * advince vector
+     */
+    class DenseGraph{
+    private:
+        int n; // count of vectrix
+        int m; // count of edge
+        bool directed; // if directed
+        vector<vector<bool>> g; // graph
+
+    public:
+        /// construction
+        /// \param n
+        /// \param directed
+        DenseGraph(int n, bool directed){
+            this->n = n;
+            this->m = 0;
+            this->directed = directed;
+
+            for (int i = 0; i < n; i++) {
+                g.push_back( vector<bool>(n, false) ); //TODO: can't understand now
+            }
+        }
+
+        ~DenseGraph(){
+            // Do nothing
+        }
+
+        int V(){ return n; }
+        int E(){ return m; }
+
+        /// confirm two v has e
+        /// \param v
+        /// \param w
+        /// \return
+        bool hasEdge(int v, int w){
+            assert(v >= 0 && v <= n);
+            assert(w >= 0 && w <= n);
+
+            return g[v][w];
+        }
+
+        ///  add edge
+        /// \param v
+        /// \param w
+        void addEdge(int v, int w) {
+            assert(v >= 0 && v <= n);
+            assert(w >= 0 && w <= n);
+
+            // 去掉了平行边的概念
+            if (hasEdge(v, w)) {
+                return;
+            }
+
+            g[v][w] = true;
+            if(!directed){
+                g[w][v] = true;
+            }
+            m++;
+            return;
+        }
+
+        /// show a dense graph
+        void show(){
+            for (int i = 0; i < V(); i++) {
+                for (int j = 0; j < V(); j++) {
+                    cout<<g[i][j]<<" ";
+                }
+                cout<<endl;
+            }
+        }
+
+
+        /*
+         *
+         */
+        class adjIterator{
+        private:
+            DenseGraph &G;
+            int v; // current vertex
+            int index;
+
+        public:
+            adjIterator(DenseGraph graph, int v) : G(graph) {
+                this->v = v;
+                this->index = -1; // 数组的下标从0开始，因此初始值可设置为 -1;
+            }
+
+            ///
+            /// \return
+            int begin(){
+                index = -1;
+                return next(); // 将index设置为-1，然后返回第一个next即可
+            }
+
+            ///
+            /// \return
+            int next(){
+                index += 1;
+                for ( ; index < G.V() ; index++) {
+                    if(G.g[v][index])
+                        return index; // 返回值为true的图的index
+                }
+                return -1;
+            }
+
+            bool end(){
+                return index >= G.V();
+            }
+        };
+    };
+}
+
+#endif //DEMO1_DENSEGRAPH_H
---
+++ cpps/demo1/WeightGraph/SparseGraph.h
@@ -0,0 +1,128 @@
+//
+// Created by 黎进 on 2018/8/31.
+//
+
+#ifndef DEMO1_SPARSEGRAPH_H
+#define DEMO1_SPARSEGRAPH_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+namespace WeightGraph{
+    /*
+     * 稀疏图，邻接表
+     */
+    class SparseGraph {
+    private:
+        int n; // count of vertex
+        int m; // count of edge
+        bool directed; // if directed
+        vector<vector<int>> g; // graph
+
+    public:
+        ///
+        /// \param n
+        /// \param directed
+        SparseGraph(int n, bool directed) {
+            this->n = n;
+            this->m = 0;
+            this->directed = directed;
+            g = vector<vector<int>>(n, vector<int>());
+        }
+
+        ~SparseGraph() {
+            // Do nothing
+        }
+
+        int V() { return n; }
+
+        int E() { return m; }
+
+        ///
+        /// \param v
+        /// \param w
+        /// \return
+        bool hasEdge(int v, int w) {
+            assert(v >= 0 && v <= n);
+            assert(w >= 0 && w <= n);
+
+            for (int i = 0; i < g[v].size(); i++) {
+                if (g[v][i] == w)
+                    return true;
+            }
+            return false;
+        }
+
+        ///
+        /// \param v
+        /// \param w
+        void addEdge(int v, int w) {
+            assert(v >= 0 && v <= n);
+            assert(w >= 0 && w <= n);
+
+            g[v].push_back(w);
+            // 处理自环边ß
+            if (v != w && !directed) {
+                g[w].push_back(v);
+            }
+            m++;
+            return;
+        }
+
+        /// show an sparse graph
+        void show(){
+            for (int i = 0; i < V(); i++) {
+                cout<<"vertex "<<i<<":\t";
+                for (int j = 0; j < g[i].size(); j++) {
+                    cout<<g[i][j]<<" ";
+                }
+                cout<<endl;
+            }
+        }
+
+        /*
+         * adjacent iterator
+         */
+        class adjIterator {
+        private:
+            SparseGraph &G;
+            int v;
+            int index;
+
+        public:
+            adjIterator(SparseGraph graph, int v) : G(graph) {
+                this->v = v;
+                this->index = 0;
+            }
+
+            ///
+            /// \return
+            int begin() {
+                index = 0;
+                if (G.g[v].size() > 0) {
+                    return G.g[v][index];
+                }
+                return -1;
+            }
+
+            ///
+            /// \return
+            int next(){
+                index ++;
+                if(index < G.g[v].size()){
+                    return G.g[v][index];
+                }
+                return -1;
+            }
+
+            ///
+            /// \return
+            bool end(){
+                return index >= G.g[v].size();
+            }
+        };
+    };
+}
+#endif //DEMO1_SPARSEGRAPH_H
